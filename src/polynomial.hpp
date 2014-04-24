490fe8be6b14fe25d35f46c3f6a6ff40be17162e
Joshua Szekely <jeszekely@gmail.com>
2014-04-24 11:39:11 -0500

    Adding Hermite functionality

--- src/polynomial.hpp
+++ src/polynomial.hpp
@@ -4,6 +4,10 @@
 #include <assert.h>
 #include <stdexcept>
 #include <vector>
+#include <algorithm>
+#include <cmath>
+#include <complex>
+#include <iostream>
 
 template <typename T>
 class polynomial
@@ -20,7 +24,7 @@ public:
   {
     std::copy_n(o.vals->data(), size, vals->data());
   }
-  polynomial(polynomial&& o) : size(o.size), vals(std::move(o.vals)){}
+  // polynomial(polynomial&& o) : size(o.size), vals(std::move(o.vals)){}
 
   T& element(const int nn)
   {
@@ -42,24 +46,110 @@ public:
     return element(nn);
   }
 
-  template <typename U>
-  polynomial operator+(const polynomial<U>& o) const
+  polynomial& operator=(const polynomial<T>& o)
   {
-    int len = std::max(size,o.size);
+    vals = std::make_shared<std::vector<T>>(o.size);
+    copy_n(&o(0),o.size,&element(0));
+    return *this;
+  }
+
+  polynomial operator+(const polynomial<T>& o) const
+  {
+    int len;
+    const polynomial<T> *p;
+    size >= o.size ? (p=this, len=size) : (p=&o, len=o.size);
     polynomial<T> out(len);
-    for (int ii = 0; ii < len; ii++)
-        out(ii) = element(ii) + o(ii) + 1;
+    std::transform(vals->data(),vals->data()+std::min(size,o.size),o.vals->data(),out.vals->data(),[](T a, T b){return a+b;});
+    if (size != o.size) std::copy_n(&p->element(std::min(size,o.size)),abs(size-o.size),&out(std::min(size,o.size)));
     return out;
   }
 
-  template <typename U>
-  polynomial& operator+=(const polynomial<U>& o)
+  polynomial& operator+=(const polynomial<T>& o)
   {
     *this = *this + o;
     return *this;
   }
 
+  polynomial operator-(const polynomial<T>& o) const
+  {
+    int len, sign;
+    int min = std::min(size,o.size);
+    const polynomial<T> *p;
+    size >= o.size ? (p=this, len=size, sign=1) : (p=&o, len=o.size, sign=-1);
+    polynomial<T> out(len);
+    std::transform(vals->data(),vals->data()+min,o.vals->data(),out.vals->data(),[](T a, T b){return a-b;});
+    if (size != o.size)
+    {
+      std::copy_n(&p->element(min),abs(size-o.size),&out(min));
+      if (sign == -1) std::transform(&out(min),&out(min)+abs(size-o.size),&out(min),[](T a){return -1.0*a;});
+    }
+    return out;
+  }
+
+  polynomial& operator-=(const polynomial<T>& o)
+  {
+    *this = *this - o;
+    return *this;
+  }
+
+  polynomial operator*(const polynomial<T>& o) const
+  {
+    int len = size+o.size-1;
+    polynomial<T> out(len);
+    for (int ii = 0; ii<size; ii++)
+    {
+      for (int jj = 0; jj<o.size; jj++)
+        out(ii+jj) += (element(ii)*o(jj));
+    }
+    return out;
+  }
+
+  polynomial& operator*=(const polynomial<T>& o)
+  {
+    *this = *this * o;
+    return *this;
+  }
+
+  template <typename U>
+  void scale(const U a)
+  {
+    std::for_each(vals->data(), vals->data()+size, [&a](T& p){p*=a;});
+    return;
+  }
+
 };
 
+template <typename T>
+std::ostream &operator<<(std::ostream &out, const polynomial<T> &o)
+{
+  for (int ii = 0; ii < o.size; ii++)
+    out << o(ii) << "\t";
+  out << endl;
+  return out;
+};
+
+template <typename T>
+std::shared_ptr<polynomial<T>> Hermite(int nn)
+{
+  std::shared_ptr<polynomial<T>> p0,p1,pn;
+  polynomial<T> a1(2);
+  a1(1) = 1;
+  p0 = make_shared<polynomial<T>>(1);
+  p0->element(0) = T(1.0);
+  p1 = make_shared<polynomial<T>>(2);
+  p1->element(1) = T(1.0);
+  if (nn == 0) return p0;
+  if (nn == 1) return p1;
+  int kk = 1;
+  while (kk < nn)
+  {
+    kk++;
+    p0->scale(kk-1);
+    *pn = (*p1)*a1-(*p0);
+    p0 = p1;
+    p1 = pn;
+  }
+  return pn;
+}
 
 #endif
